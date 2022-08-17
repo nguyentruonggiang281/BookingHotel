d91c9be7ec44f4a896c56a48777b4c9dfa6a50e8
dinhtn0204 <19130042@st.hcmuaf.edu.vn>
2022-08-18 00:25:18 +0700

    update web HomeController...

---
+++ src/main/java/com/res/web/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.res.web.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.res.dao.TypeRoomDao;
+import com.res.model.TypeRoom;
+
+@WebServlet(value = "/trang-chu")
+public class HomeController extends HttpServlet{
+
+	private TypeRoomDao typeRoomDao = new TypeRoomDao();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<TypeRoom> listTypeRoom = typeRoomDao.findAll();
+		req.setAttribute("listTypeRoom", listTypeRoom);
+		RequestDispatcher rd = req.getRequestDispatcher("/views/index.jsp");
+		rd.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+	
+}
